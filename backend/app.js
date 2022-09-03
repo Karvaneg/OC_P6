359d853c53021187e83db415e56e3a0f5bd8fa36
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-09-03 12:29:51 +0200

    Mise en place du serveur de développement Node + gestion des erreurs CORS + connexion à MongoDB
    localhost:3000 et localhost:4200 fonctionnent

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://user81:piiquante@cluster0.iwnujfs.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+// CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+// Routes attendues par le frontend
+
+
+module.exports = app;
